a8cd4d984df460a2ed9b99bfb70096229d7417cf
IbrahimYunuslu <ibrahimyunuslu@gmail.com>
2025-03-06 18:38:14 +0400

    Added name of the class loader

--- Main.java
+++ Main.java
@@ -8,6 +8,7 @@ public class Main {
             Class<?> myClass = customClassLoader.loadClass("CustomClass");
             Object instance = myClass.getDeclaredConstructor().newInstance();
             System.out.println("Class loaded: " + myClass.getName());
+            System.out.println("Class loader: " + myClass.getClassLoader());
         } catch (Exception e) {
             System.err.println("Error loading or instantiating class: " + e.getMessage());
         }
